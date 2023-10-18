501ccd42801a71296e9c711b79752e3e48a19cad
Sergey <146075682+juss9@users.noreply.github.com>
2023-10-18 12:40:20 +0300

    реализовали корректный формат вывода данных задачи и сделали индикацию нажатия кнопки категории

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -4,24 +4,27 @@ import {Category} from "../../model/Category";
 
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+    selector: 'app-categories',
+    templateUrl: './categories.component.html',
+    styleUrls: ['./categories.component.css']
 })
 
 export class CategoriesComponent implements OnInit {
-  categories: Category[]
-  constructor(private dataHandler: DataHandlerService) {
-  }
+    selectedCategory: Category
+    categories: Category[]
 
-  ngOnInit() {
-    this.dataHandler.categoriesSubject.subscribe(categories => this.categories = categories);
+    constructor(private dataHandler: DataHandlerService) {
+    }
 
-  }
+    ngOnInit() {
+        this.dataHandler.categoriesSubject.subscribe(categories => this.categories = categories);
 
-  showTasksByCategory(category: Category) {
-    this.dataHandler.fillTaskByCategory(category)
+    }
 
-  }
+    showTasksByCategory(category: Category) {
+        this.selectedCategory = category
+        this.dataHandler.fillTaskByCategory(category)
+
+    }
 }
 
