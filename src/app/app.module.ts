10524bc11075e8b0c48340f03995bb36b6f83b3a
Sergey <146075682+juss9@users.noreply.github.com>
2023-10-19 20:27:30 +0300

    Изменение вывода таблицы с задачами с помощью MatTable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,28 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import { CategoriesComponent } from './views/categories/categories.component';
-import { TasksComponent } from './views/tasks/tasks.component';
+import {CategoriesComponent} from './views/categories/categories.component';
+import {TasksComponent} from './views/tasks/tasks.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        CategoriesComponent,
-        TasksComponent
-    ],
-    imports: [
-        BrowserModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    CategoriesComponent,
+    TasksComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatTableModule,
+    MatSortModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,23 +1,43 @@
 import {Component, OnInit} from '@angular/core';
 import {DataHandlerService} from "../../service/data-handler.service";
 import {Task} from "../../model/Task";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements  OnInit{
+export class TasksComponent implements OnInit {
+  displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category']
+  dataSource: MatTableDataSource<Task>
+
   tasks: Task[]
+
   constructor(private dateHandler: DataHandlerService) {
   }
 
   ngOnInit() {
     this.dateHandler.taskSubject.subscribe(tasks => this.tasks = tasks)
+    this.dataSource = new MatTableDataSource()
+    this.refreshTable();
   }
 
   toggleTaskCompleted(task: Task) {
     task.completed = !task.completed
 
+  }
+
+   getPriorityColor(task: Task){
+    if( task.completed){
+      return '#F8F9FA'
+    }
+    if (task.priority && task.priority.color){
+      return task.priority.color
+    }
+    return '#fff '
+  }
+  private refreshTable(){
+    this.dataSource.data = this.tasks
   }
 }
