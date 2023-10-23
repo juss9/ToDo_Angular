af9399bbdeef7ed33323367b800b6db89d277727
Sergey <146075682+juss9@users.noreply.github.com>
2023-10-23 09:59:33 +0300

    промежуточной результат страницы

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,43 +1,104 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {DataHandlerService} from "../../service/data-handler.service";
-import {Task} from "../../model/Task";
-import {MatTableDataSource} from "@angular/material/table";
+import {Task} from 'src/app/model/Task';
+
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
-  displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category']
-  dataSource: MatTableDataSource<Task>
+export class TasksComponent implements OnInit, AfterViewInit {
 
-  tasks: Task[]
+  // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
+   displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+   dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
 
-  constructor(private dateHandler: DataHandlerService) {
+  // ссылки на компоненты таблицы
+  @ViewChild(MatPaginator, {static: false}) private paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) private sort: MatSort;
+
+
+  tasks: Task[];
+
+  constructor(private dataHandler: DataHandlerService) {
   }
 
   ngOnInit() {
-    this.dateHandler.taskSubject.subscribe(tasks => this.tasks = tasks)
-    this.dataSource = new MatTableDataSource()
+    this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
+
+    // датасорс обязательно нужно создавать для таблицы, в него присваивается любой источник (БД, массивы, JSON и пр.)
+    this.dataSource = new MatTableDataSource();
+
     this.refreshTable();
   }
 
+  // в этом методе уже все проинциализировано, поэтому можно присваивать объекты (иначе может быть ошибка undefined)
+  ngAfterViewInit(): void {
+
+    this.addTableObjects();
+
+  }
+
+
   toggleTaskCompleted(task: Task) {
-    task.completed = !task.completed
+    task.completed = !task.completed;
+  }
+
+  // в зависимости от статуса задачи - вернуть цвет названия
+   getPriorityColor(task: Task) {
+
+    // цвет завершенной задачи
+    if (task.completed) {
+      return '#F8F9FA'; // TODO вынести цвета в константы (magic strings, magic numbers)
+    }
+
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+
+    return '#fff'; // TODO вынести цвета в константы (magic strings, magic numbers)
 
   }
 
-   getPriorityColor(task: Task){
-    if( task.completed){
-      return '#F8F9FA'
-    }
-    if (task.priority && task.priority.color){
-      return task.priority.color
-    }
-    return '#fff '
+  // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
+  private refreshTable() {
+
+    this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
+
+    this.addTableObjects();
+
+
+    // когда получаем новые данные..
+    // чтобы можно было сортировать по столбцам "категория" и "приоритет", т.к. там не примитивные типы, а объекты
+    // @ts-ignore - показывает ошибку для типа даты, но так работает, т.к. можно возвращать любой тип
+    this.dataSource.sortingDataAccessor = (task, colName) => {
+
+      // по каким полям выполнять сортировку для каждого столбца
+      switch (colName) {
+        case 'priority': {
+          return task.priority ? task.priority.id : null;
+        }
+        case 'category': {
+          return task.category ? task.category.title : null;
+        }
+        case 'date': {
+          return task.date ? task.date : null;
+        }
+
+        case 'title': {
+          return task.title;
+        }
+      }
+    };
+
   }
-  private refreshTable(){
-    this.dataSource.data = this.tasks
+
+  private addTableObjects() {
+    this.dataSource.sort = this.sort; // компонент для сортировки данных (если необходимо)
+    this.dataSource.paginator = this.paginator; // обновить компонент постраничности (кол-во записей, страниц)
   }
 }
